8844cd4c502741fe8aa5fe8b7f5fe369fa606ff5
donksly <okello.kepha@gmail.com>
2018-08-21 23:04:05 +0300

    config migration vars 2

--- database/migrations/2018_08_21_174431_sales_invoice_grid.php
+++ database/migrations/2018_08_21_174431_sales_invoice_grid.php
@@ -15,29 +15,29 @@ class SalesInvoiceGrid extends Migration
     {
         Schema::create('sales_invoice_grid', function (Blueprint $table){
             $table->increments('entity_id');
-            $table->string('increment_id')->unsigned()->index();
+            $table->string('increment_id');
             $table->string('state');
-            $table->string('store_id')->default(0);
+            $table->string('store_id');
             $table->string('store_name');
-            $table->string('order_id')->unsigned()->index();
-            $table->string('order_increment_id')->unsigned()->index();
-            $table->string('order_created_at')->unsigned()->index();
-            $table->string('customer_name')->unsigned()->index();
-            $table->string('customer_email')->unsigned()->index();
+            $table->string('order_id');
+            $table->string('order_increment_id');
+            $table->string('order_created_at');
+            $table->string('customer_name');
+            $table->string('customer_email');
             $table->integer('customer_group_id');
             $table->string('payment_method');
             $table->string('store_currency_code');
             $table->string('order_currency_code');
             $table->string('base_currency_code');
             $table->string('global_currency_code');
-            $table->string('billing_name')->unsigned()->index();
-            $table->string('billing_address')->unsigned()->index();
-            $table->string('shipping_address')->unsigned()->index();
+            $table->string('billing_name');
+            $table->string('billing_address');
+            $table->string('shipping_address');
             $table->string('shipping_information');
             $table->decimal('subtotal', 12,4);
             $table->decimal('shipping_and_handling', 12,4);
             $table->decimal('grand_total', 12,4);
-            $table->decimal('base_grand_total', 12,4)->unsigned()->index();
+            $table->decimal('base_grand_total', 12,4);
             $table->timestamps();
         });
     }
