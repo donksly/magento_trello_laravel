0102a29f899280050e28bab3d308170454195e9a
donksly <okello.kepha@gmail.com>
2018-08-22 21:55:23 +0300

    Trello boards api tests

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -314,7 +314,8 @@ class OrdersController extends Controller
         $boardID = "";
         $listID = "";
         $boards = $client->api('member')->boards()->all("me", array());
-        Log::info($boards[0]); ////////////BOARD A
+        Log::info('Supplier A'.json_encode($boards[0])); ////////////BOARD A
+        Log::info('Supplier B'.json_encode($boards[1])); ////////////BOARD A
         foreach ($boards as $board) {
 
             Log::info(json_encode($board));
