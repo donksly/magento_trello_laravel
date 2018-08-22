d53b645ed0070e7e8a77923bdd61ec0b755f49a9
donksly <okello.kepha@gmail.com>
2018-08-22 17:29:46 +0300

    OAuth client api configs - Laravel Socialite

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('')->group(function () {
+Route::group(['middleware' => ['web']], function () {
     Route::any('/trellotoken', 'OrdersController@fetchTrelloToken');
 });
