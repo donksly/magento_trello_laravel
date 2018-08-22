e33c3f9410188d6d3596103e2382ca631be06dd4
donksly <okello.kepha@gmail.com>
2018-08-22 16:15:06 +0300

    OAuth client api configs - Laravel Socialite

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,8 +17,8 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\Event' => [
             'App\Listeners\EventListener',
         ],
-        SocialiteWasCalled::class => [
-            'SocialiteProviders\\Trello\\TrelloExtendSocialite@handle',
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            'SocialiteProviders\Trello\TrelloExtendSocialite@handle',
         ],
     ];
 
