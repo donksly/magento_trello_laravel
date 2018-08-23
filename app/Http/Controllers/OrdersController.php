ec98645353c0f2a1a0853492a1213a834a278359
donksly <okello.kepha@gmail.com>
2018-08-23 18:42:14 +0300

    Trello try bulk initial post

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -380,21 +380,21 @@ class OrdersController extends Controller
         if(sizeof($final_card)==0){
             foreach($all_orders as $order){
             //push all from our db
-                Log::info($order->supplier_id);
-            $get_supplier = $this->suppliers[$order->supplier_id];
-            $get_status_name = $helper->getCurrentOrderStatus($order->status);
-            $card_name = $helper->formatOrderNumberForView($order->sales_order_id);
-            $card_description = 'Created on: Order by: '.$helper->formatDateTimeWithSeconds($order->created_at);
+            if($order->supplier_id != 0 ){
+                $get_supplier = $this->suppliers[$order->supplier_id];
+                $get_status_name = $helper->getCurrentOrderStatus($order->status);
+                $card_name = $helper->formatOrderNumberForView($order->sales_order_id);
+                $card_description = 'Created on: Order by: '.$helper->formatDateTimeWithSeconds($order->created_at);
             }
-            $close_default = 'false';
+                $close_default = 'false';
 
-            $card
-                ->setBoard($get_supplier)
-                ->setList($get_status_name)
-                ->setName($card_name)
-                ->setDescription($card_description)
-                ->save();
-
+                $card
+                    ->setBoard($get_supplier)
+                    ->setList($get_status_name)
+                    ->setName($card_name)
+                    ->setDescription($card_description)
+                    ->save();
+            }
         }else{
             //validate and sync pulled now validate update and push i.e update
             //foreach
